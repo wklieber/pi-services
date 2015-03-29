2a03e63a9380adfa0e74e6943b4b8f1d5d30b9fe
Werner Klieber <wklieber@gmail.com>
2015-03-29 13:07:00 +0200

    script

--- pi/modules/services/src/test/resources/wklieber/start_service.sh
+++ pi/modules/services/src/test/resources/wklieber/start_service.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cd ~/git/pi-services/pi/modules/services
+cd ~/git/pi-services
+git pull
+
+cd pi/modules/services
 mvn clean package
 java -jar target/pi-services-1.0-SNAPSHOT.jar server default-configuration.yml
 
